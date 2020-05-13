a208106d16c7722dc87bcc4cb5db0faf5e026dd5
Mathias Buus <mathiasbuus@gmail.com>
2020-05-13 20:05:37 +0200

    fix standard

--- test.js
+++ test.js
@@ -23,13 +23,13 @@ tape('batch', function (t) {
   const a = Buffer.alloc(32)
   const b = Buffer.alloc(32)
 
-  blake2b.batch(a, [ Buffer.from('hi'), Buffer.from('ho') ])
-  blake2b.batch(b, [ Buffer.from('hi'), Buffer.from('ho') ])
+  blake2b.batch(a, [Buffer.from('hi'), Buffer.from('ho')])
+  blake2b.batch(b, [Buffer.from('hi'), Buffer.from('ho')])
 
   t.same(a, b)
 
   b.fill(0)
-  browserBlake2b.batch(b, [ Buffer.from('hi'), Buffer.from('ho') ])
+  browserBlake2b.batch(b, [Buffer.from('hi'), Buffer.from('ho')])
 
   t.same(a, b)
 
